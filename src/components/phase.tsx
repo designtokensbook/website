4d2ff6cf4713f2f8813e6073aee1a22820c69f55
Dorsaf Frigui <dorsaffrigui@MBP-de-Dorsaf.lan>
2024-03-21 09:48:49 +0100

    ui improvements

--- src/components/phase.tsx
+++ src/components/phase.tsx
@@ -1,25 +1,26 @@
 import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
-} from "@/components/ui/accordion"
-import type { Phase } from "@/types/phase"
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import type { Phase } from "@/types/phase";
 
-
-export default function PhaseSection({id, theme, phases}: Phase ) {
-	return (
-		<Accordion type="single" collapsible className="w-full">
-				<AccordionItem key={id} value={id}>
-					<AccordionTrigger>{theme}</AccordionTrigger>
-					<AccordionContent>
-                        <ul className="list-disc pl-8">
-                        {phases.map((phase) => 
-                        <li className="text-muted-foreground mb-3">{phase}</li>
-                        )}
-                        </ul>
-                        </AccordionContent>
-				</AccordionItem>
-		</Accordion>
-	)
+export default function PhaseSection({ id, theme, phases }: Phase) {
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem key={id} value={id}>
+        <AccordionTrigger>{theme}</AccordionTrigger>
+        <AccordionContent>
+          <ul className="list-disc pl-8">
+            {phases.map((phase) => (
+              <li key={phase} className="text-muted-foreground mb-3">
+                {phase}
+              </li>
+            ))}
+          </ul>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
 }
