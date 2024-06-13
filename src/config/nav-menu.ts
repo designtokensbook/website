8af5256b45ee18fb4b913d856cdaf9a22d087a3c
Adrien Gibrat <agibrat@frontguys.fr>
2024-06-13 13:22:32 +0200

    publish verbatims & cases

--- src/config/nav-menu.ts
+++ src/config/nav-menu.ts
@@ -8,14 +8,16 @@ export const navMenuConfig: NavMenuConfig = {
         {
           title: "Téléchargez le livre numérique",
           href: "https://frontguys.fr/the-design-tokens-book/",
-          description: "Téléchargez gratuitement le livre au format PDF sur Fronguys.fr",
+          description:
+            "Téléchargez gratuitement le livre au format PDF sur Fronguys.fr",
           external: true,
         },
         {
           title: "Achetez le livre imprimé",
           href: "https://link.designtokensbook.com/buy",
-          description: "Impression couleur premium, format 16x24 cm sur Amazon.fr",
-          external: true
+          description:
+            "Impression couleur premium, format 16x24 cm sur Amazon.fr",
+          external: true,
         },
         {
           title: "Assistez aux meetups",
@@ -33,26 +35,26 @@ export const navMenuConfig: NavMenuConfig = {
         {
           title: "Liens",
           href: "/links",
-          description: "Retrouvez les liens vers les ressources citées dans le livre.",
+          description:
+            "Retrouvez les liens vers les ressources citées dans le livre.",
         },
         {
           title: "Ateliers",
           href: "/workshops",
-          description: "Des ateliers pour mettre en place les tokens dans votre organisation.",
+          description:
+            "Des ateliers pour mettre en place les tokens dans votre organisation.",
         },
         {
           title: "Retours d'experience",
           href: "/cases",
           description:
             "Les retours d'experience de plusieurs équipes Design et Développement.",
-          disabled: true
         },
         {
           title: "Points de vue",
           href: "/verbatims",
           description:
             "Des experts livrent leurs reflexions sur les Design Tokens.",
-          disabled: true
         },
       ],
     },
